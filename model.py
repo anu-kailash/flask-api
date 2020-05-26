4cd3b8e6882dde989a38f507a3ceaae8a542bf62
aryavj0893 <65354347+aryavj0893@users.noreply.github.com>
2020-05-26 15:16:09 +0530

    included session add

--- model.py
+++ model.py
@@ -1,14 +1,35 @@
-from sqlalchemy import create_engine, Column, Integer,NVARCHAR
+from sqlalchemy import Column, Integer, NVARCHAR, ForeignKey
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-engine=create_engine("sqlite:///chinook.db", echo=True)
+from sqlalchemy.orm import sessionmaker
 Base=declarative_base()
+engine=create_engine('sqlite:///chinook.db', echo=True)
 
 
-class Albums(Base):
+class Album(Base):
     __tablename__ = 'albums'
-    AlbumId=Column(Integer,primary_key=True,autoincrement=True, nullable=True)
-    Title=Column(NVARCHAR, nullable=True)
-    ArtistId=Column(Integer, nullable=True)
+    AlbumId=Column(Integer, primary_key=True,nullable=True, autoincrement=True)
+    Title=Column(NVARCHAR(160), nullable=True)
+    ArtistId=Column(Integer,ForeignKey('artists.ArtistId'), nullable=True, )
 
 
 Base.metadata.create_all(engine)
+
+
+class Artists(Base):
+    __tablename__ = 'artists'
+    ArtistId=Column(Integer, primary_key=True,nullable=True, autoincrement=True)
+    Name=Column(NVARCHAR(120), nullable=True)
+
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind = engine)
+session = Session()
+#c1 = Artists(Name = 'Vaishya')
+
+#session.add(c1)
+#session.commit()
+result = session.query(Album).all()
+for row in result:
+   print ("Title: ",row.Title, "Artist ID:",row.ArtistId)
