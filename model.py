0f0b399b5d4050fbbf2ea78241acfb243730f1a1
Linto9605 <lintosanna03@gmail.com>
2020-06-03 10:56:08 +0530

    add model class

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -80,19 +79,3 @@ Base.metadata.create_all(engine)
 
 
 
-=======
-from sqlalchemy import create_engine, Column, Integer,NVARCHAR
-from sqlalchemy.ext.declarative import declarative_base
-engine=create_engine("sqlite:///chinook.db", echo=True)
-Base=declarative_base()
-
-
-class Albums(Base):
-    __tablename__ = 'albums'
-    AlbumId=Column(Integer,primary_key=True,autoincrement=True, nullable=True)
-    Title=Column(NVARCHAR, nullable=True)
-    ArtistId=Column(Integer, nullable=True)
-
-
-Base.metadata.create_all(engine)
->>>>>>> 75a8b17a51d07dd034c1b340a328df9056dc6c86
